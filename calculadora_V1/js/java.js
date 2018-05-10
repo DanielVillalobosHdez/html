c7bd0f8b0aa5e41e9fa02f5f540c1ca085815a65
danielvillaloboshdez <danivillalobos1997@gmail.com>
2018-05-10 11:14:00 +0200

    calculadora

--- calculadora_V1/js/java.js
+++ calculadora_V1/js/java.js
@@ -7,6 +7,7 @@ function Obtenerdatos() {
 	var dato1;
 	var dato2;
 	var operacion;
+	var potencia;
 
 	dato1 = parseInt(document.getElementById('Dato1').value);
 	dato2 = parseInt(document.getElementById('Dato2').value);
@@ -16,15 +17,45 @@ function Obtenerdatos() {
 	switch (operacion[operacion.selectedIndex].value) {
 		case "a":
 			resultado.innerHTML = dato1 + "+" + dato2 + " = " + (dato1 + dato2); 
+			if(dato1 < 0)
+				resultado.innerHTML = "(" + dato1 + ")" + "+" + dato2 + " = " + (dato1 + dato2);
+			if(dato2 < 0)
+				resultado.innerHTML = dato1 + "+" + "(" + dato2 + ")" + " = " + (dato1 + dato2);
+			if(dato1 < 0 && dato2 < 0)
+				resultado.innerHTML = "(" + dato1 + ")" + "+" + "(" + dato2 + ")" + " = " + (dato1 + dato2);
 			break;
 		case "b":
 		    resultado.innerHTML = dato1 + "-" + dato2 + " = " + (dato1 - dato2);
+		    if(dato1 < 0)
+				resultado.innerHTML = "(" + dato1 + ")" + "-" + dato2 + " = " + (dato1 - dato2);
+			if(dato2 < 0)
+				resultado.innerHTML = dato1 + "-" + "(" + dato2 + ")" + " = " + (dato1 - dato2);
+			if(dato1 < 0 && dato2 < 0)
+				resultado.innerHTML = "(" + dato1 + ")" + "-" + "(" + dato2 + ")" + " = " + (dato1 - dato2); 
 			break;
 		case "c":
-			resultado.innerHTML = dato1 + "x" + dato2 +  " = " + (dato1 * dato2); 
+			resultado.innerHTML = dato1 + "x" + dato2 +  " = " + (dato1 * dato2);
+			if(dato1 < 0)
+				resultado.innerHTML = "(" + dato1 + ")" + "x" + dato2 + " = " + (dato1 * dato2);
+			if(dato2 < 0)
+				resultado.innerHTML = dato1 + "x" + "(" + dato2 + ")" + " = " + (dato1 * dato2);
+			if(dato1 < 0 && dato2 < 0)
+				resultado.innerHTML = "(" + dato1 + ")" + "x" + "(" + dato2 + ")" + " = " + (dato1 * dato2); 
 			break;
 		case "d":
 			resultado.innerHTML = dato1 + "/" + dato2 + " = " + (dato1 / dato2); 
+			if(dato1 < 0)
+				resultado.innerHTML = "(" + dato1 + ")" + "/" + dato2 + " = " + (dato1 / dato2);
+			if(dato2 < 0)
+				resultado.innerHTML = dato1 + "/" + "(" + dato2 + ")" + " = " + (dato1 / dato2);
+			if(dato1 < 0 && dato2 < 0)
+				resultado.innerHTML = "(" + dato1 + ")" + "/" + "(" + dato2 + ")" + " = " + (dato1 / dato2); 
+			break;
+		case "e":
+			resultado.innerHTML = dato1 + "<sup>" + dato2 + "</sup>" + " = " + Math.pow(dato1, dato2);
+			break;
+		case "f":
+			resultado.innerHTML = "<sup>" + dato1 + "</sup>" + "&#8730;" + dato2 + " = " + Math.sqrt(dato1, dato2);
 			break;
 	}
 }
--- calculadora_V2/js/java.js
+++ calculadora_V2/js/java.js
@@ -1,7 +1,42 @@
-function Calcular() {
+var textbox;
+var numero1;
+var numero2;
 
-	var num1, operando, num2, contador;
-
+function poner(numero) {
 	
+	var operando;
+	
+	textbox = document.getElementById('valor');
+
+	textbox.value += numero;
+
+	if(numero.value.length = 1)
+		numero1 = parseInt(numero);
+	if(numero.value.length = 3)
+		numero2 = parseInt(numero);
 	
 }
+function Calcular() {
+	
+	var resultado;
+	var textbox;
+	var operando;
+
+	operando = document.getElementById('oper').value;
+	textbox = document.getElementById('valor');
+
+	switch (operando) {
+		case 'a':
+			resultado = (numero1 + numero2);
+		case 'b':
+			resultado = (numero1 - numero2);
+		case 'c':
+			resultado = (numero1 * numero2);
+		case 'c':
+			resultado = (numero1 / numero2);
+	}
+
+	textbox.value = resultado;
+
+}
+
