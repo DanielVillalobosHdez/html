1811a9da16687db22be49a22e0f16bf777354d8b
DanielVillalobosHdez <danivillalobos1997@gmail.com>
2018-05-15 09:31:35 +0200

    while and do...while

--- demo6/js/javascript.js
+++ demo6/js/javascript.js
@@ -3,12 +3,25 @@ function EscribirNumero() {
 	var campoResultado, numero, contador;
 
 	campoResultado = document.getElementById('Resultado');
-	numero = parseInt(document.getElementById('numero').value);
+	numero = parseInt(document.getElementById('Numero').value);
 
 	contador = 0;
 
 	//numero del 1 al numero 
 	// primero con while
 	// luego con do while
+
+	while (contador < numero){
+		campoResultado.innerHTML += contador + ' ';
+		contador++;
+	}
+
+	contador = 0;
+	campoResultado.innerHTML += '<br>';
+
+	do {
+		campoResultado.innerHTML += contador + ' ';
+		contador++;
+	} while (contador < numero);
 
 }
