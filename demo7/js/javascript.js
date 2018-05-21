2eeefd839c77b6c1a7eff80fdda52eec842827a7
danielvillaloboshdez <danivillalobos1997@gmail.com>
2018-05-21 13:33:42 +0200

    Agenda

--- demo7/js/javascript.js
+++ demo7/js/javascript.js
@@ -1,4 +1,5 @@
 class Contacto {
+	
 	constructor(n, t){
 		this.nombre = n;
 		this.telefono = t;
@@ -8,22 +9,49 @@ class Contacto {
 		var resultado;
 
 		resultado = document.getElementById('Resultado');
+		resultado.innerHTML += "<br>";
 		resultado.innerHTML += 
 			"Nombre: " + this.nombre + "<br>" +
-			"Teléfono" + this.telefono + "<br>";
+			"Teléfono: " + this.telefono + "<br><br>";
 	}
 
 	CambiarTelefono(MrT){
 		this.telefono = MrT;
 	}
+
+	CambiarNombre(MrT){
+		this.nombre = MrT;
+	}
 }
 
+var c1, c2, c3;
+
 function CrearContacto() {
-	var c1 = new Contacto('Pepe', 1111),
-		c2 = new Contacto('Ana', 2222);
+	c1 = new Contacto('Pepe', 1111),
+	c2 = new Contacto('Ana', 2222),
+	c3 = new Contacto('Josefa', 3333);
 
 	c1.Escribir();
 	c2.Escribir();
+	c3.Escribir();
+
 }
 
-//imprimir tres contactos y poder modificar los contactos
+function Modificar() {
+	var valorNombre,
+		valorTelefono;
+
+	valorNombre = document.getElementById('Nombre').value;
+	valorTelefono = document.getElementById('Telefono').value;
+
+	c1.CambiarNombre(valorNombre);
+	c1.CambiarTelefono(valorTelefono);
+
+	c1.Escribir();
+	c2.Escribir();
+	c3.Escribir();
+
+
+}
+
+//5 contactos, añador, modificar, borrar y buscar
